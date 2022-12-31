d241b83b4c2f639dfd3c9e785f322bc25b17af4a
sevde <sevdebetulsecer@eskisehir.edu.tr>
2022-12-31 10:45:54 +0300

    new assignment.

---
+++ src/Exercises/CengStudent.java
@@ -0,0 +1,47 @@
+package Exercises;
+
+import java.time.LocalDate;
+
+public class CengStudent {
+    private static final String DEPARTMENT = "Computer Engineering";
+    private final String id;
+    private final String fullName;
+    private LocalDate birthDate;
+    private int semester;
+
+    public CengStudent(String id,String fullName){
+        this.id = id;
+        this.fullName = fullName;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+    public int getSemester() {
+        return semester;
+    }
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+    public boolean isBirthday() {
+        if (birthDate != null) {
+            LocalDate now = LocalDate.now();
+            return now.getMonth() == birthDate.getMonth() &&
+                    now.getDayOfMonth() == birthDate.getDayOfMonth();
+        }
+        else
+            return false;
+    }
+    static String getDepartment() {
+        return DEPARTMENT;
+    }
+}
+
